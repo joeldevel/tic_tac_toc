b26f200c7a1fbcd18b949a6d90b1de9208afd458
joeldevel <christianjoel.developer@gmail.com>
2020-08-06 19:22:24 -0300

    Added style to square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-// squre receives props, here destructured
+// An object containing the square style
+const style = {
+    background: 'dodgerblue',
+    border: '2px solid black',
+    fontSize: '30px',
+    fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none'
+};
+
+// Square receives props, here destructured
 // the button uses those props
 const Square = ( {value, onClick}) => (
-  <button onClick={onClick}>
+  <button onClick={onClick} style={style}>
     { value }
   </button>
 )
