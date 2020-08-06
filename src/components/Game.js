90cc0faa057e460a2311fc0af22c6446c5185b97
joeldevel <christianjoel.developer@gmail.com>
2020-08-06 20:07:11 -0300

    Fully functional game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import Board from './Board';
 import {calculateWinner} from '../helpers';
-
+const styles = {
+  width: '200px',
+  display:'inline-block',
+  color: 'dodgerblue',
+  margin:'0 auto'
+}
 const Game = () => {
   // use hooks, the initial state of board is a 9 null array
   const [ board, setBoard] = useState(Array(9).fill(null));
@@ -22,10 +27,18 @@ const Game = () => {
 
   }
   const renderMoves = () => {
-
+    return <button onClick={() => setBoard(Array(9).fill(null))}>
+               Start Game
+    </button>
   }
   return (
-    <Board squares={board} onClick={handleClick}/>
+    <>
+      <Board squares={board} onClick={handleClick}/>
+      <div style={styles}>
+          <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
+          {renderMoves()}
+      </div>
+    </>
   )
 }
 
